da6097d15d08914443602e9d9730194b57678deb
Joseph makau <125541346+McJose@users.noreply.github.com>
2024-06-14 17:06:00 +0300

    Delete JMICT.js

--- JMICT.js
+++
@@ -1,7 +0,0 @@
-const drop = document.querySelector('#mobile-menu');
-const dropitems = document.querySelector('.menu');
-
-drop.addEventListener('click', function() {
-    drop.classList.toggle('is-active');
-    dropitems.classList.toggle('active');
-});
